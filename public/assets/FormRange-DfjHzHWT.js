460b41b27fe2a42f2590506779c0c1ba3a7f9f78
hafizab63861462 <hafiz.abdullahoffical@gmail.com>
2025-11-16 06:59:15 +0500

    latest build

--- public/assets/FormRange-DfjHzHWT.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as n,j as p,l as c}from"./index-CJhCcwq4.js";import{F as x}from"./FormContext-ozGyXaUG.js";const f=o.forwardRef(({bsPrefix:r,className:a,id:s,...t},e)=>{const{controlId:m}=o.useContext(x);return r=n(r,"form-range"),p.jsx("input",{...t,type:"range",ref:e,className:c(a,r),id:s||m})});f.displayName="FormRange";export{f as F};
